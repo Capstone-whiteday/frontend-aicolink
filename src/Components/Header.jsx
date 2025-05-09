aa6bf092643c406d5507f5f59e2a63d5e5039b5c
fellix519 <daydreaming519@naver.com>
2025-05-09 20:00:46 +0900

    get menu buttons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,13 @@
 const Header =()=>{
     return(
         <header>
-            header
+            <div>
+                <button>AICOLINK</button>
+                <button>월말정산</button>
+                <button>내 충전소</button>
+                <button>도움말</button>
+                <button>서비스 소개</button>
+            </div>
         </header>
     );
 }
