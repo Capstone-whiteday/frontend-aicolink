893a19b243cbc73db08ff20bc10b4b6a7c022212
fellix519 <daydreaming519@naver.com>
2025-05-09 21:27:09 +0900

    got logo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,22 +1,24 @@
 import './Header.css';
 
 const Header = () => {
-    return (
-      <header className="header">
-        <div className="logo">AICOLINK</div>
-        <nav className="nav">
-          <a href="#">월말정산</a>
-          <a href="#">내 충전소</a>
-          <a href="#">도움말</a>
-          <a href="#">서비스 소개</a>
-          <div className="icons">
-            <button className="icon-button">🔔</button>
-            <button className="icon-button">⚙️</button>
-          </div>
-        </nav>
-      </header>
-    );
-  };
-  
+  return (
+    <header className="header">
+      <div className="logo-container">
+        <img src="/logo_aclnk.svg" alt="AICOLINK Logo" className="logo-icon" />
+        <span className="logo-text">AICOLINK</span>
+      </div>
+      <nav className="nav">
+        <a href="#">월말정산</a>
+        <a href="#">내 충전소</a>
+        <a href="#">도움말</a>
+        <a href="#">서비스 소개</a>
+        <div className="icons">
+          <button className="icon-button">🔔</button>
+          <button className="icon-button">⚙️</button>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
