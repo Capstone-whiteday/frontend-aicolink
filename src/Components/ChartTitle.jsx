65a0ab2bcd7345ca40e58850d9f04f62d705a40f
fellix519 <daydreaming519@naver.com>
2025-05-09 21:55:40 +0900

    before get rid of ChartTitle.jsx

--- src/Components/ChartTitle.jsx
+++ src/Components/ChartTitle.jsx
@@ -1,10 +1,10 @@
 const ChartTitle =()=>{
     const charger_name="tmpName"
-    const date="date_tmp"
+    const date="undefined"
     return(
         <>
-            <h1 padding="50px"> charger_name</h1>
-            <h4>date</h4>
+            <h1 padding="50px"></h1>
+            <h4>tmp</h4>
         </>
     );
 }
