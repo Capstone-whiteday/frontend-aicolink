0ee96d9a001c299e081e7f7d325d914d05a34ec6
Subin Choi <125132754+subi930@users.noreply.github.com>
2025-05-31 17:54:11 +0900

    fix: set output directory for build in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',  // nginx에서 경로 잘 읽히도록 설정
+  build: {
+    outDir: 'dist',
+  },
   server: {
     port: 5173,
     proxy: {
