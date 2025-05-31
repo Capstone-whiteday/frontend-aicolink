3e9e2ea4b794d503f7a99c1651617513cd6bf6d3
Subin Choi <125132754+subi930@users.noreply.github.com>
2025-05-31 17:10:46 +0900

    fix: set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : '/',
   server: {
     port: 5173, // 원하는 포트 번호
   },
